521fbb6aedc6ae91cdc8c3b7039cb713df16d76f
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-10 09:12:32 +0530

    feat: setup error handling modules

---
+++ src/lib/api/error/applicationError.js
@@ -0,0 +1,14 @@
+class ApplicationError extends Error {
+  constructor(options, errors, overrides = {}) {
+    super();
+    this.message = options.message;
+    this.name = 'ApplicationError';
+    this.statusCode = options.statusCode;
+    this.code = options.code;
+    if (errors) this.errors = errors;
+    Object.assign(this, overrides);
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = ApplicationError;
