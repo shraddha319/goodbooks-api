04926a425face925cb6e2b5717f6288fdf8a6804
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-11 02:51:31 +0530

    feat: mount cart, wishlist on /users/:id route

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -12,24 +12,23 @@ const {
   getUserById,
   updateUserById,
   deleteUserById,
-  token,
 } = require('../validations/user.validation');
+const cartRouter = require('./cart.route');
+const wishlistRouter = require('./wishlist.route');
 
 const router = express.Router();
 
 router.route('/').post(validate(createUser, 'User'), catchAsync(postUser));
 
-router.use(
-  '/:userId',
-  validate(token),
-  catchAsync(validateUserId),
-  catchAsync(tokenVerifier),
-);
+router.use('/:userId', catchAsync(validateUserId), tokenVerifier);
 
 router
   .route('/:userId')
   .get(validate(getUserById, 'User'), getUser)
   .post(validate(updateUserById, 'User'), catchAsync(updateUser))
   .delete(validate(deleteUserById, 'User'), catchAsync(deleteUser));
+
+router.use('/:userId/cart', cartRouter);
+router.use('/:userId/wishlist', wishlistRouter);
 
 module.exports = router;
