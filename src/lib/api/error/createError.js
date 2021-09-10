42c1fa943ae80409ae8db0a96d918b9b3decc727
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-11 02:52:28 +0530

    fix: remove comments

--- src/lib/api/error/createError.js
+++ src/lib/api/error/createError.js
@@ -5,27 +5,16 @@ const ErrorTypes = require('./types');
 
 const createError = (err, overrides = {}) => {
   let options;
-  // let validationErrors;
 
-  // if (err instanceof mongoose.Error.ValidationError) {
-  //   options = ErrorTypes.VALIDATION_ERROR;
-  //   validationErrors = getMongooseValidationErrors(err.errors);
-  // }
   if (err instanceof mongoose.Error.CastError) {
     options = ErrorTypes.INVALID_ID;
   } else if (err instanceof jwt.JsonWebTokenError) {
     options = ErrorTypes.UNAUTHORIZED;
   } else options = ErrorTypes.SERVER_ERROR;
   if (err.message) options.message = err.message;
-
+  console.log(err);
   const error = new ApplicationError(options, overrides);
   return error;
 };
 
 module.exports = createError;
-
-// const getMongooseValidationErrors = (errors) =>
-//   Object.keys(errors).reduce((res, key) => {
-//     const { message, type } = errors[key].properties;
-//     return [...res, { key, message, type }];
-//   }, []);
